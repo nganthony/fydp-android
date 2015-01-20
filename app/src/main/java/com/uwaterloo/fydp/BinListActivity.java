7df84cb0df973440395f895b47c5679b5056218c
anthonyng <ngcanthony@gmail.com>
2015-01-20 16:56:30 -0500

    Added activity to display list of bins

---
+++ app/src/main/java/com/uwaterloo/fydp/BinListActivity.java
@@ -0,0 +1,81 @@
+package com.uwaterloo.fydp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.app.Activity;
+
+
+/**
+ * An activity representing a list of Bins. This activity
+ * has different presentations for handset and tablet-size devices. On
+ * handsets, the activity presents a list of items, which when touched,
+ * lead to a {@link BinDetailActivity} representing
+ * item details. On tablets, the activity presents the list of items and
+ * item details side-by-side using two vertical panes.
+ * <p/>
+ * The activity makes heavy use of fragments. The list of items is a
+ * {@link BinListFragment} and the item details
+ * (if present) is a {@link BinDetailFragment}.
+ * <p/>
+ * This activity also implements the required
+ * {@link BinListFragment.Callbacks} interface
+ * to listen for item selections.
+ */
+public class BinListActivity extends Activity
+        implements BinListFragment.Callbacks {
+
+    /**
+     * Whether or not the activity is in two-pane mode, i.e. running on a tablet
+     * device.
+     */
+    private boolean mTwoPane;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bin_list);
+
+        if (findViewById(R.id.bin_detail_container) != null) {
+            // The detail container view will be present only in the
+            // large-screen layouts (res/values-large and
+            // res/values-sw600dp). If this view is present, then the
+            // activity should be in two-pane mode.
+            mTwoPane = true;
+
+            // In two-pane mode, list items should be given the
+            // 'activated' state when touched.
+            ((BinListFragment) getFragmentManager()
+                    .findFragmentById(R.id.bin_list))
+                    .setActivateOnItemClick(true);
+        }
+
+        // TODO: If exposing deep links into your app, handle intents here.
+    }
+
+    /**
+     * Callback method from {@link BinListFragment.Callbacks}
+     * indicating that the item with the given ID was selected.
+     */
+    @Override
+    public void onItemSelected(String id) {
+        if (mTwoPane) {
+            // In two-pane mode, show the detail view in this activity by
+            // adding or replacing the detail fragment using a
+            // fragment transaction.
+            Bundle arguments = new Bundle();
+            arguments.putString(BinDetailFragment.ARG_ITEM_ID, id);
+            BinDetailFragment fragment = new BinDetailFragment();
+            fragment.setArguments(arguments);
+            getFragmentManager().beginTransaction()
+                    .replace(R.id.bin_detail_container, fragment)
+                    .commit();
+
+        } else {
+            // In single-pane mode, simply start the detail activity
+            // for the selected item ID.
+            Intent detailIntent = new Intent(this, BinDetailActivity.class);
+            detailIntent.putExtra(BinDetailFragment.ARG_ITEM_ID, id);
+            startActivity(detailIntent);
+        }
+    }
+}
